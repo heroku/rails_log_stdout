1bd9cb000139ee312b0fe70447da3820fbeb4a49
Matt Bray <mattjbray@gmail.com>
2012-11-02 19:18:31 +0000

    Set logger before frameworks initialize
    Ensures ActiveRecord, ActionController and ActionMailer loggers bind to
    the new logger.

--- lib/rails_log_stdout/railtie.rb
+++ lib/rails_log_stdout/railtie.rb
@@ -2,7 +2,7 @@ require 'rails_log_stdout/rails3'
 
 module RailsLogStdout
   class Railtie < ::Rails::Railtie
-    config.after_initialize do
+    config.before_initialize do
       Rails3.set_logger(config)
     end
   end
